2331c98130f1c79152832ea55b21a7e4cacdb12b
SharmaRahul111 <sharmarahul111.psr@gmail.com>
2026-01-15 10:24:05 +0545

    Thinking of fast and better way to send settings

---
+++ panels.js
@@ -0,0 +1,15 @@
+class Panels {
+  constructor(setting) {
+    this.root = document.createElement("div.panels")
+    this.setting = setting
+    document.body.appendChild(this.root)
+
+  }
+}
+
+// slider -> 
+// field -> "string"
+// option -> [array]
+// checkbox -> true/false
+// button -> function()
+// color -> ...
